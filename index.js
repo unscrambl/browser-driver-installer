de74b8c0115b53a42f5252f50b6787d21ac326ca
Caner Dagli <caner@unscrambl.com>
2018-05-02 10:06:38 +0300

    Formatter fixes applied

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+
 const program = require('commander');
 const driverInstaller = require('./installer').driverInstaller;
 
@@ -6,7 +7,8 @@ program
     .option('--chrome-version [chromeVersion]', 'Chrome browser major version string e.g. 65')
     .option('--chrome-driver-target-path [chromeDriverTargetPath]', 'Path to install Chrome driver executable')
     .option('--firefox-version [firefoxVersion]', 'Firefox browser major version string e.g. 57')
-    .option('--firefox-driver-target-path [firefoxDriverTargetPath]', 'Path to install Firefox driver(geckoDriver) executable')
+    .option('--firefox-driver-target-path [firefoxDriverTargetPath]',
+        'Path to install Firefox driver(geckoDriver) executable')
     .parse(process.argv);
 
 driverInstaller(program.chromeVersion, program.chromeDriverTargetPath, program.firefoxVersion, program.firefoxDriverTargetPath);
--- installer.js
+++ installer.js
@@ -1,4 +1,4 @@
-'use strict'; 
+'use strict';
 /* eslint-disable no-console */
 
 var runNpmChildProcess = require('./runNpmChildProcess');
@@ -6,31 +6,33 @@ var path = require('path');
 var execSync = require('child_process').execSync;
 var shell = require('shelljs');
 
-const TEMP_DIR = 'temp'; 
+const TEMP_DIR = 'temp';
 const CHROME_DRIVER_NAME = 'chromedriver';
 const CHROME_DRIVER_BIN_PATH = path.join('node_modules', 'chromedriver', 'lib', 'chromedriver', CHROME_DRIVER_NAME);
 const CHROME_DRIVER_VERSION_REGEX = new RegExp(/\w+ ([0-9]+.[0-9]+).+/);
-const GECKO_DRIVER_NAME = 'geckodriver'; 
+const GECKO_DRIVER_NAME = 'geckodriver';
 const GECKO_DRIVER_BIN_PATH = path.join('node_modules', 'geckodriver', GECKO_DRIVER_NAME);
 const GECKO_DRIVER_VERSION_REGEX = new RegExp(/\w+\s(\d+.\d+.\d+)/);
 const BROWSER_MAJOR_VERSION_REGEX = new RegExp(/^(\d+)/);
 
 function installDriverWithVersion(driverName, driverBinPath, installPath, versionObject)
 {
-    if(checkDirectoryAndVersion(driverName, installPath, versionObject.driverVersion))
+    if (checkDirectoryAndVersion(driverName, installPath, versionObject.driverVersion))
     {
-        return; 
+        return;
     }
 
     shell.mkdir('-p', TEMP_DIR);
 
-    runNpmChildProcess(['install', `${driverName}@${versionObject.driverNPMPackageVersion}`, '--prefix',  TEMP_DIR]).then(function ()
+    runNpmChildProcess(['install', `${driverName}@${versionObject.driverNPMPackageVersion}`, '--prefix', TEMP_DIR]).then(
+        function ()
         {
             shell.mkdir('-p', installPath);
             console.log('package dependencies have been installed');
-            shell.cp('-n',path.join(TEMP_DIR, driverBinPath), installPath);
+            shell.cp('-n', path.join(TEMP_DIR, driverBinPath), installPath);
             shell.rm('-rf', TEMP_DIR);
-        }, function (e)
+        },
+        function (e)
         {
             console.log('package dependencies installation failed with error, details: ' + e.toString());
         });
@@ -38,50 +40,59 @@ function installDriverWithVersion(driverName, driverBinPath, installPath, versio
 
 function checkDirectoryAndVersion(driverName, installPath, driverExpectedVersion)
 {
-    if (!shell.test('-e', installPath)) {
-        return false; 
+    if (!shell.test('-e', installPath))
+    {
+        return false;
     }
     console.log(`Directory '${installPath}' does exist.`);
     console.log(`Checking if the directory contains a ${driverName}...`);
 
-    if(!shell.test('-e', path.join(installPath, driverName))){
-        console.log(`Could not find the ${driverName} in the directory '${installPath}'. Attempting to install it...`); 
-        return false; 
-    }
-    
-    console.log(`${driverName} found.`);   
-    const driverMajorVersion = driverVersionString(driverName, installPath); 
-    if(driverMajorVersion !== driverExpectedVersion)
+    if (!shell.test('-e', path.join(installPath, driverName)))
     {
-        console.log(`${driverName} expected version (${driverExpectedVersion}) does not match with the installed version (${driverMajorVersion}).`);
+        console.log(`Could not find the ${driverName} in the directory '${installPath}'. Attempting to install it...`);
+        return false;
+    }
+
+    console.log(`${driverName} found.`);
+    const driverMajorVersion = driverVersionString(driverName, installPath);
+    if (driverMajorVersion !== driverExpectedVersion)
+    {
+        console.log(
+            `${driverName} expected version (${driverExpectedVersion}) does not match with the installed version (${driverMajorVersion}).`
+        );
         console.log('Removing the old version...');
         shell.rm(path.join(installPath, driverName));
-        return false; 
+        return false;
     }
-    
+
     console.log(`${driverName} version ${driverExpectedVersion} has already been installed!`);
-    return true;  
+    return true;
 }
 
 function driverVersionString(driverName, installPath)
 {
-    let versionOutput = null; 
-    if(driverName === CHROME_DRIVER_NAME){
+    let versionOutput = null;
+    if (driverName === CHROME_DRIVER_NAME)
+    {
         versionOutput = execSync(path.join(installPath, driverName) + ' -v').toString();
         return versionOutput.match(CHROME_DRIVER_VERSION_REGEX)[1];
-    } else if (driverName === GECKO_DRIVER_NAME)
+    }
+    else if (driverName === GECKO_DRIVER_NAME)
     {
         versionOutput = execSync(path.join(installPath, driverName) + ' -V').toString();
         return versionOutput.match(GECKO_DRIVER_VERSION_REGEX)[1];
-    } else {
+    }
+    else
+    {
         throw new Error(`No driver exists with the name ${driverName}.`);
     }
 }
 
-function driverInstaller(detectedChromeVersion, chromeDriverTargetPath, detectedFirefoxVersion, geckoDriverTargetPath){
+function driverInstaller(detectedChromeVersion, chromeDriverTargetPath, detectedFirefoxVersion, geckoDriverTargetPath)
+{
     const browserVersionsObject = JSON.parse(shell.cat(path.resolve(__dirname, 'driverVersions.json')));
     // ChromeDriver NPM package versions are defined according to https://github.com/giggio/node-chromedriver/releases
-    const chromeDriverVersions = browserVersionsObject.chromeDriverVersions; 
+    const chromeDriverVersions = browserVersionsObject.chromeDriverVersions;
 
     // GeckoDriver NPM package versions are defined according to https://github.com/mozilla/geckodriver/releases
     const geckoDriverVersions = browserVersionsObject.geckoDriverVersions;
@@ -89,16 +100,28 @@ function driverInstaller(detectedChromeVersion, chromeDriverTargetPath, detected
     detectedChromeVersion = majorBrowserVersion(detectedChromeVersion);
     detectedFirefoxVersion = majorBrowserVersion(detectedFirefoxVersion);
 
-    if(detectedChromeVersion && !chromeDriverVersions[detectedChromeVersion]){
-        console.log(`Failed to locate a version of ChromeDriver that matches the installed version of Chrome (${detectedChromeVersion}). Valid Chrome versions are: ${Object.keys(chromeDriverVersions).join(', ')}`)
-    } else if(detectedChromeVersion){ 
-        installDriverWithVersion(CHROME_DRIVER_NAME, CHROME_DRIVER_BIN_PATH, chromeDriverTargetPath, chromeDriverVersions[detectedChromeVersion]);
+    if (detectedChromeVersion && !chromeDriverVersions[detectedChromeVersion])
+    {
+        console.log(
+            `Failed to locate a version of ChromeDriver that matches the installed version of Chrome (${detectedChromeVersion}). Valid Chrome versions are: ${Object.keys(chromeDriverVersions).join(', ')}`
+        );
+    }
+    else if (detectedChromeVersion)
+    {
+        installDriverWithVersion(CHROME_DRIVER_NAME, CHROME_DRIVER_BIN_PATH, chromeDriverTargetPath,
+            chromeDriverVersions[detectedChromeVersion]);
     }
 
-    if(detectedFirefoxVersion && !geckoDriverVersions[detectedFirefoxVersion]){
-        console.log(`Failed to locate a version of GeckoDriver that matches the installed version of Firefox (${detectedFirefoxVersion}). Valid Firefox versions are: ${Object.keys(geckoDriverVersions).join(', ')}`)
-    } else if(detectedFirefoxVersion){ 
-        installDriverWithVersion(GECKO_DRIVER_NAME, GECKO_DRIVER_BIN_PATH, geckoDriverTargetPath, geckoDriverVersions[detectedFirefoxVersion])
+    if (detectedFirefoxVersion && !geckoDriverVersions[detectedFirefoxVersion])
+    {
+        console.log(
+            `Failed to locate a version of GeckoDriver that matches the installed version of Firefox (${detectedFirefoxVersion}). Valid Firefox versions are: ${Object.keys(geckoDriverVersions).join(', ')}`
+        );
+    }
+    else if (detectedFirefoxVersion)
+    {
+        installDriverWithVersion(GECKO_DRIVER_NAME, GECKO_DRIVER_BIN_PATH, geckoDriverTargetPath, geckoDriverVersions[
+            detectedFirefoxVersion]);
     }
 }
 
