bc2843c8102d80aa7e9a070b45f8205400aafc34
Caner Dagli <caner@unscrambl.com>
2018-05-02 12:17:06 +0300

    Added more tests

--- installer.spec.js
+++ installer.spec.js
@@ -8,37 +8,69 @@ const path = require('path');
 
 chai.use(chaiSinon);
 
-describe('browserDriverInstaller', function(){
-    const DEFAULT_TIMEOUT_IN_MILLIS = 60000; 
-    const DRIVER_OUTPUT_PATH = './output'; 
+describe('browserDriverInstaller', function ()
+{
+    const DEFAULT_TIMEOUT_IN_MILLIS = 60000;
+    const DRIVER_OUTPUT_PATH = './output';
 
     this.timeout(DEFAULT_TIMEOUT_IN_MILLIS);
 
-    beforeEach(function (){
+    beforeEach(function ()
+    {
         sinon.spy(console, 'log');
     });
 
-    afterEach(function() {
-        console.log.restore();
-    });
-
-    xit('should not attempt to install anything if one of the path, version or both parameters are not provided', function()
+    afterEach(function ()
     {
-        expect(installer.driverInstaller()).to.be.false;
-        expect(console.log).to.have.been.calledWith('No Chrome version or target path is provided. Skipping...');
-        expect(console.log).to.have.been.calledWith('No Firefox version or target path is provided. Skipping...');
+        console.log.restore();
+        cleanTheOutput();
     });
 
-    xit('should throw an error if the provided version does not included in the JSON file', function(){
-        const wrongVersionNumber = '1'; 
-        expect(function () { installer.driverInstaller(wrongVersionNumber, '/some/target/path'); } ).to.throw(/Failed to locate a version of ChromeDriver that matches the installed version of Chrome \(1\). Valid Chrome versions are:*/);
+    function cleanTheOutput()
+    {
+        shell.rm('-rf', DRIVER_OUTPUT_PATH);
+    }
+
+    it('should not attempt to install anything if one of the path, version or both parameters are not provided',
+        function ()
+        {
+            expect(installer.driverInstaller()).to.be.false;
+            expect(console.log).to.have.been.calledWith(
+                'No Chrome version or target path is provided. Skipping...');
+            expect(console.log).to.have.been.calledWith(
+                'No Firefox version or target path is provided. Skipping...');
+        });
+
+    it('should throw an error if the provided version does not included in the JSON file', function ()
+    {
+        const wrongVersionNumber = '1';
+        expect(function () { installer.driverInstaller(wrongVersionNumber, '/some/target/path'); }).to.throw(
+            /Failed to locate a version of ChromeDriver that matches the installed version of Chrome \(1\). Valid Chrome versions are:*/
+        );
     });
 
-    it('should install the chromedriver', function(done){
-        installer.driverInstaller('54', './output').then(function(){
-            expect(shell.test('-e', path.resolve(DRIVER_OUTPUT_PATH, 'chromedriver'))).to.be.true;
-            shell.rm('-rf', DRIVER_OUTPUT_PATH);
-            done();
+    it('should install the chromedriver to specified path if the version is included in the JSON file',
+        function ()
+        {
+            return installer.driverInstaller('54', DRIVER_OUTPUT_PATH).then(function ()
+            {
+                expect(shell.test('-e', path.resolve(DRIVER_OUTPUT_PATH, 'chromedriver'))).to.be.true;
+            });
+        });
+
+    it('should install the geckodriver to specified path if the version is included in the JSON file', function ()
+    {
+        return installer.driverInstaller(null, null, '55', DRIVER_OUTPUT_PATH).then(function ()
+        {
+            expect(shell.test('-e', path.resolve(DRIVER_OUTPUT_PATH, 'geckodriver'))).to.be.true;
         });
     });
-});  
+
+    it('should not install again if the wanted version is already installed', function ()
+    {
+        return installer.driverInstaller('54', DRIVER_OUTPUT_PATH).then(function ()
+        {
+            expect(installer.driverInstaller('54', DRIVER_OUTPUT_PATH)).to.be.false;
+        });
+    });
+});
