41462d8024765906ea7913ec758d33ae0b2f53d4
S. Ahmet Bolac <ahmet@unscrambl.com>
2021-05-28 15:59:20 +0300

    Fix command line parsing

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const browserDriverInstaller = require('./installer').browserDriverInstaller;
 
 if (require.main === module)
 {
-    program
+    const options = program
         .option('--browser-name <browser-name>', 'Sets the name of the browser to install the driver for')
         .option('--browser-version <browser-version>', 'Sets the browser version string, e.g., 65, 67.0.23')
         .option('--target-path <target-path>', 'Sets the target path to install the driver executable')
         .parse(process.argv)
         .opts();
 
-    browserDriverInstaller(program.browserName, program.browserVersion, program.targetPath);
+    browserDriverInstaller(options.browserName, options.browserVersion, options.targetPath);
 }
 else
 {
